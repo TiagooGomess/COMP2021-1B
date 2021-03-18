2f0a96e1145a3fa4ebbeadedd1c0ca5e3d02996e
Tiago Gomes <up201806658@fe.up.pt>
2021-03-18 00:37:09 +0000

    Started expression operators implementation

--- ex.sh
+++ ex.sh
@@ -1,3 +1,3 @@
 rm -Rf generated/
 gradle build
-java -cp "./build/classes/java/main/:./libs/utils.jar" JmmCompiler "fail/syntactical/BlowUp"
+java -cp "./build/classes/java/main/:./libs/utils.jar" JmmCompiler "Simple"
