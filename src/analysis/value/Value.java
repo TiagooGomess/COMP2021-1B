769aa149c064788f0605d123b7a965260b763145
a3brx <a3brx@tuta.io>
2021-04-29 19:24:33 +0100

    invokespecial in its own line

--- src/analysis/value/Value.java
+++ src/analysis/value/Value.java
@@ -40,7 +40,7 @@ public abstract class Value {
         }
 
         ArrayList<String> childLines = new ArrayList<>(Arrays.asList(ollir.split("\n")));
-        String lastLine = childLines.get(childLines.size() - 1).replace("%VariableName", terminal.getOllir());
+        String lastLine = childLines.get(childLines.size() - 1).replace("%VariableName", terminal.getOllir()).replace("; invokespecial", ";\ninvokespecial");
         childLines.remove(childLines.size() - 1);
         String assignmentType = Value.typeToOllir(operand.getReturnType());
         result.insert(0, terminal.getOllir() + " :=" + assignmentType + " " + lastLine + ";\n");
